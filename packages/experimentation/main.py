ab49ae7c2c127d489f8fc4755b0c1c20e63d743c
AlexC <alexchau203@gmail.com>
2023-05-31 23:59:13 +1000

    moved jupyter to main,py

--- packages/experimentation/main.py
+++ packages/experimentation/main.py
@@ -3,10 +3,44 @@ import tekore as tk
 
 from collections import Counter
 
-conf = tk.config_from_environment()
-scope = tk.scope.playlist_read_private
-token = tk.prompt_for_user_token(*conf, scope=scope)
+# conf = tk.config_from_environment()
+# scope = tk.scope.playlist_read_private
+# token = tk.prompt_for_user_token(*conf, scope=scope)
 
+conf = tk.config_from_environment()
+token = tk.prompt_for_user_token(*conf)
+spotify = tk.Spotify(token)
+
+spotify.current_user()
+current_user = spotify.current_user().uri
+
+for i, playlist in enumerate(spotify.playlists('gingerale3').items):
+    if playlist.owner.uri != spotify.current_user().uri:
+        continue
+    print(f"Idx {i}, name: {playlist.name}", )
+    for j in playlist.images:
+        print(f"height: {j.height}, width: {j.width}, url: {j.url}")
+
+    print("---------------------------")
+
+
+    
+
+
+playlist = spotify.followed_playlists(limit=1).items[0]
+track = spotify.playlist_items(playlist.id, limit=1).items[0].track
+name = f'"{track.name}" from {playlist.name}'
+
+if track.episode:
+    print(f'Cannot analyse episodes!\nGot {name}.')
+elif track.track and track.is_local:
+    print(f'Cannot analyse local tracks!\nGot {name}.')
+else:
+    print(f'Analysing {name}...\n')
+    analysis = spotify.track_audio_features(track.id)
+    print(repr(analysis))
+
+    
 
 def get_artist(track) -> str:
     if track.episode:

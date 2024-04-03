d444dfb2fe017f66bb1bd639b906b70b7e89fc4d
alexchaucs <alexchau203@gmail.com>
2024-04-03 22:37:32 +1100

    Move classes into base __init__ file

--- packages/api/src/services/__init__.py
+++ packages/api/src/services/__init__.py
@@ -1,3 +1,11 @@
 from src.services.spotifyAuth import SpotifyAuth
+from src.services.spotifyPlaylists import Playlists
 
 spotify_auth = SpotifyAuth()
+
+__all__ = [
+    SpotifyAuth,
+    spotify_auth,
+    Playlists,
+]
+

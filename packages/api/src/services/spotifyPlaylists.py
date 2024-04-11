d3d690848a8a1582ac947e1b8be8510b34b4fb76
alexchaucs <alexchau203@gmail.com>
2024-04-11 20:36:26 +1000

    Improve readability of class to pull playlists data

--- packages/api/src/services/spotifyPlaylists.py
+++ packages/api/src/services/spotifyPlaylists.py
@@ -4,111 +4,70 @@ import html
 
 class Playlists:
     def __init__(self, token):
-        self.spotify = tk.Spotify(token = token, asynchronous=True)
+        self.spotify = tk.Spotify(token=token, asynchronous=True)
 
     async def current_user(self):
         return await self.spotify.current_user()
 
-
     async def get_playlists_follow(self):
-        return await self.spotify.followed_playlists() #followed_playlists
+        return await self.spotify.followed_playlists()
+
+    async def _fetch_all_playlists(self, user=None):
+        """Fetch all playlists, optionally filtered by a user."""
+        playlists = []
+        response = await self.spotify.followed_playlists(limit=1)
+        total = response.total
+        playlists.extend(response.items)
+        
+        for offset in range(1, total, 10):
+            response = await self.spotify.followed_playlists(limit=10, offset=offset)
+            playlists.extend(response.items)
+
+        if user:
+            playlists = [p for p in playlists if p.owner.uri == user]
+        
+        return playlists
 
     async def get_playlists_user(self, user):
-        playlists = []
-        inital_response = await self.spotify.followed_playlists(limit = 1, offset = 0)
-        total = inital_response.total 
-        playlists.extend(inital_response.items)
-        limit = 10
-        numOfCalls = (total - 1)//limit + 1
-        
-        tasks = [self.spotify.followed_playlists(limit = limit, offset = 1 + limit * i) for i in range(numOfCalls)]
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            playlists.extend(response.items)
-
-        userPlaylists = []
-        for _, playlist in enumerate(playlists):
-            if playlist.owner.uri != user:
-                continue
-            userPlaylists.append(playlist)
-
-        return userPlaylists
-
+        return await self._fetch_all_playlists(user)
 
     async def get_playlists_data(self, user):
-        playlists = []
-        inital_response = await self.spotify.followed_playlists(limit = 1, offset = 0)
-        total = inital_response.total 
-        playlists.extend(inital_response.items)
-        limit = 10
-        numOfCalls = (total - 1)//limit + 1
-        
-        tasks = [self.spotify.followed_playlists(limit = limit, offset = 1 + limit * i) for i in range(numOfCalls)]
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            playlists.extend(response.items)
+        playlists = await self._fetch_all_playlists(user)
+        return [{
+            "name": p.name,
+            "url": p.images[0].url if p.images else None,
+            "description": html.unescape(p.description) if p.description else '',
+            "id": p.id
+        } for p in playlists]
 
-        userPlaylistsData = []
-        for _, playlist in enumerate(playlists):
-            if playlist.owner.uri != user:
-                continue
-            userPlaylistsData.append({
-                "name": playlist.name,
-                "url": playlist.images[0].url,
-                "description": html.unescape(playlist.description),
-                "id": playlist.id
-                })
+    async def _fetch_all_playlist_items(self, playlistID):
+        """Fetch all items of a playlist."""
+        items = []
+        response = await self.spotify.playlist_items(playlistID, limit=1)
+        total = response.total
+        items.extend(response.items)
 
-        return userPlaylistsData
+        for offset in range(1, total, 100):
+            response = await self.spotify.playlist_items(playlistID, limit=100, offset=offset)
+            items.extend(response.items)
 
-    async def get_playlist_tracks(self, playlistID: str):
-        playlistData = []
-        inital_response = await self.spotify.playlist_items(playlistID, limit = 1, offset = 0)
-        total = inital_response.total 
-        playlistData.extend(inital_response.items)
-        limit = 100
-        numOfCalls = (total - 1)//limit + 1
-        
-        tasks = [self.spotify.playlist_items(playlistID, limit = limit, offset = 1 + limit * i) for i in range(numOfCalls)]
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            playlistData.extend(response.items)
+        return items
 
-        return playlistData
+    async def get_playlist_tracks(self, playlistID):
+        return await self._fetch_all_playlist_items(playlistID)
 
+    async def get_playlist_tracks_ids(self, playlistID):
+        items = await self._fetch_all_playlist_items(playlistID)
+        return [item.track.id for item in items if item.track]
 
-    async def get_playlist_tracks_ids(self, playlistID: str):
-        playlistTracks = []
-        inital_response = await self.spotify.playlist_items(playlistID, limit = 1, offset = 0)
-        total = inital_response.total 
-        playlistTracks.extend(inital_response.items)
-        limit = 100
-        numOfCalls = (total - 1)//limit + 1
-        
-        tasks = [self.spotify.playlist_items(playlistID, limit = limit, offset = 1 + limit * i) for i in range(numOfCalls)]
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            playlistTracks.extend(response.items)
+    async def get_tracks_audio_features(self, tracks):
+        features = []
+        for i in range(0, len(tracks), 50):
+            response = await self.spotify.tracks_audio_features(tracks[i:i+50])
+            features.extend(response)
+        return features
 
-        return [track.track.id for track in playlistTracks]
-
-    async def get_tracks_audio_features(self, tracks: list):
-        trackFeatures = []
-        total = len(tracks)
-        limit = 50
-        numOfCalls = (total)//limit + 1
-        
-        tasks = [self.spotify.tracks_audio_features(tracks[i*limit : (i + 1) * limit]) for i in range(numOfCalls)]
-        responses = await asyncio.gather(*tasks)
-        for response in responses:
-            trackFeatures.extend(response)
-
-        return trackFeatures
-
-
-    async def get_playlist_tracks_audio_features(self, playlistID: str):
-        # TODO - implement work and queue (sempaphore and task queue) to prevent to chain gather requests
+    async def get_playlist_tracks_audio_features(self, playlistID):
         trackIds = await self.get_playlist_tracks_ids(playlistID)
-        trackFeatures = await self.get_tracks_audio_features(trackIds)
-        
-        return [track.__dict__ for track in trackFeatures if track is not None]
+        features = await self.get_tracks_audio_features(trackIds)
+        return [feature.__dict__ for feature in features if feature]
